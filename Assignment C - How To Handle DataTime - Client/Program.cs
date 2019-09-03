c123faafa7907c491091080d7cea84d4543c6a4b
Morten <morto1995@hotmail.com>
2019-09-03 09:35:22 +0200

    DateTime

---
+++ Assignment C - How To Handle DataTime - Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment_C___How_To_Handle_DataTime___Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Start();
+        }
+    }
+}
---
+++ Assignment C - How To Handle DataTime - Server/Worker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Assignment_C___How_To_Handle_DataTime___Server
+{
+    internal class Worker
+    {
+        public void Start()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Server");
+
+            TcpListener server = new TcpListener(IPAddress.Loopback, 3003);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    TcpClient tempSocket = socket;
+                    DoClient(tempSocket);
+                });
+            }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                    Console.WriteLine("Calc...");
+
+                    string[] numbers = line.Split(' ');
+
+                    try
+                    {
+                        DateTime result = DateTime.ParseExact(numbers[0] + " " + numbers[1], "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
+                        Console.WriteLine("Valid");
+
+                        sw.WriteLine("Valid");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+
+                        Console.WriteLine("Not Valid");
+
+                        sw.WriteLine("Not Valid");
+                    }
+
+                    Console.WriteLine("Result send...");
+
+                    sw.Flush();
+                }
+            }
+        }
+    }
+}
